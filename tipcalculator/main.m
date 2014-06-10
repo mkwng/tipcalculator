f098b746b11cb8bc36d8bb34923aa7b86716fe0c
Michael Wang <mkw@fb.com>
2014-06-10 11:19:02 -0700

    Initial Commit

---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Michael Wang on 6/10/14.
+//  Copyright (c) 2014 mkwng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
