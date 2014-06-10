5d05b39a3378f03754f980ebb3280353c111f893
Michael Wang <hello@mkwng.com>
2014-06-10 11:54:01 -0700

    Tutorial completed

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Michael Wang on 6/10/14.
+//  Copyright (c) 2014 mkwng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
